3197dc604c7769226627ede3bae224edb44687d8
JMA10101 <jmandreoli@gmail.com>
2024-05-14 10:47:53 -0400

    five levels with colors

--- Helix Jump/Assets/Camera.cs
+++ Helix Jump/Assets/Camera.cs
@@ -6,6 +6,8 @@ public class Camera : MonoBehaviour
 {
     public GameObject ball;
     public GameObject camera;
+    public float yOffset = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +17,6 @@ public class Camera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        camera.transform.position=new Vector3(transform.position.x, ball.transform.position.y, transform.position.z);
+        camera.transform.position=new Vector3(transform.position.x, ball.transform.position.y+yOffset, transform.position.z);
     }
 }
