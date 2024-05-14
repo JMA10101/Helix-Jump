6837297e3fc97ebe5c266d1238f2dea94dfebb3d
JMA10101 <jmandreoli@gmail.com>
2024-05-13 23:41:01 -0400

    camera and rotating floors

--- Helix Jump/Assets/Tower Turning.cs
+++ Helix Jump/Assets/Tower Turning.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class TowerTurning : MonoBehaviour
 {
+    public float speed = 5.0f;
+    public GameObject layer;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,7 @@ public class TowerTurning : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Vector3 v3 = new Vector3(0.0f, 0.0f, Input.GetAxis("Horizontal"));
+        layer.transform.Rotate(0.0f, Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0.0f);
     }
 }
