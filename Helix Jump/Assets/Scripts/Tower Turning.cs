09559f870b4cb6582f8a33b64eec0dca89159981
JMA10101 <jmandreoli@gmail.com>
2024-05-15 23:46:42 -0400

    changed movement a little

--- Helix Jump/Assets/Scripts/Tower Turning.cs
+++ Helix Jump/Assets/Scripts/Tower Turning.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class TowerTurning : MonoBehaviour
 {
-    public float speed = 5.0f;
+    public float speed = 200.0f;
+    public float sens = 50.0f;
     public GameObject layer;
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,6 @@ public class TowerTurning : MonoBehaviour
     void Update()
     {
         //Vector3 v3 = new Vector3(0.0f, 0.0f, Input.GetAxis("Horizontal"));
-        layer.transform.Rotate(0.0f, Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0.0f);
+        layer.transform.Rotate(0.0f, -1.0f*Input.GetAxis("Mouse X") * speed*sens* Time.deltaTime, 0.0f);
     }
 }
